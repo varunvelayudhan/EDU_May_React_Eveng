07c6f082b304441a5f9dd5ca1a09915834acada2
varunvelayudhan <varu.knr@gmail.com>
2020-09-20 06:35:58 +0200

    trail commit

---
+++ build/precache-manifest.7ffa37fce630e4514c0439f9be925600.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ac9d9499c315c4b237e5a1ae116187",
+    "url": "/index.html"
+  },
+  {
+    "revision": "600e83ffec63dbbd4903",
+    "url": "/static/css/main.1c447c74.chunk.css"
+  },
+  {
+    "revision": "a2c77ea27fba99f5f623",
+    "url": "/static/js/2.3bd9e2d6.chunk.js"
+  },
+  {
+    "revision": "600e83ffec63dbbd4903",
+    "url": "/static/js/main.ae92bd7d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
